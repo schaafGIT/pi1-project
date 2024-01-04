5be4fc2f88a7d582a6f0cfe0507655ae7f2f656a
ballosh <150835935+ballosh@users.noreply.github.com>
2024-01-04 23:31:28 +0100

    Update askUser.c
    Der Datentyp bool wurde aus der Funktion herausgenommen und durch char ersetzt. Zudem wurden die Stellen mit den Variablen, die den Typen bool genutzt haben, überarbeitet. Nun funktionieren die Variablen mit den Datentyp char.
    
    Zudem wurde auch hier die Kommentierung überprüft.

--- src/askUser.c
+++ src/askUser.c
@@ -4,24 +4,23 @@
 // Autoren:
 //              Basher Allosh / 11224028
 //              Oliver Schaaf / 11225476
-// Datum: 10.12.2023
+// Datum: 04.01.2024
 // Version: 1.0
 
 #include <stdio.h>
 #include <string.h>
-#include "stdbool.h"
 
-bool askUser()
+char askUser()
 {
-    char input; // Variable für die Eingabe des Users
-    bool flag = true;
-    bool result; //
+    char input;    // Variable für die Eingabe des Users
+    char flag = 1; // Flag für die Überprüfung, ob ein zulässiger Wert eingegeben wurde
+    char result;   // Zur Speicherung der Usereingabe als Wahrheitswert
 
-    // Erstmaliges Fragen an den User, ob der Median oder Mittelwert genutzt werden soll
+    // Erstmalige Anfragen an den User, ob der Median oder Mittelwert genutzt werden soll
     printf("\n     [   1 - Mittelwert   |   2 - Median   ]\n");
     printf("\nSoll für die Auswertung der Median oder der Mittelwert der letzten drei Messwerte genutzt werden?\n");
     printf("\n  Antwort: ");
-    scanf("%s", &input);
+    scanf("%c", &input);
 
     // Überprüfung der Eingabe
     do
@@ -29,21 +28,22 @@ bool askUser()
         if (input == '1')
         {
             // Mittelwert
-            result = false;
-            flag = false;
+            result = 1;
+            flag = 0;
         }
         else if (input == '2')
         {
             // Median
-            result = true;
-            flag = false;
+            result = 0;
+            flag = 0;
         }
         else
         {
             // Wenn die Eingabe unzulässig ist
             printf("\nDie Eingabe ist unzulässig! Bitte geben Sie etwa 1 für den Mittelwert oder 2 für den Median an.\n");
             printf("\nAntwort: ");
-            scanf("%c", &input);
+            scanf("%c", &input); // Das Erste scanf() dient zum Abfangen der Entertaste, da ansonsten der Text doppelt auftritt
+            scanf("%c", &input); // Zum erneuten Einlesen der Eingabe
         }
 
     } while (flag);
